c4781f446efb759a778b52371a8c1c7cd33edfce
espg <refuge@rocktalus.com>
2024-10-08 13:21:18 -0400

    regression on setup.py to debug github builds...

--- setup.py
+++ setup.py
@@ -10,50 +10,6 @@
 
 from setuptools import setup
 
-import os
-
-#lib_folder = os.path.dirname(os.path.realpath(__file__))
-#requirement_path = f"{lib_folder}/requirements.txt"
-#install_requires = []
-
-#if os.path.isfile(requirement_path):
-#    with open(requirement_path) as f:
-#        install_requires = f.read().splitlines()
-#install_requires=install_requires,
-
 if __name__ == "__main__":
-    setup(scripts=['com/StationInfoEdit.py',
-                   'com/ArchiveService.py',
-                   'com/GenerateKml.py',
-                   'com/PlotETM.py',
-                   'com/AlterETM.py',
-                   'com/CloseStationInfo.py',
-                   'com/ConvertDate.py',
-                   'com/ConvertTrimble.py',
-                   'com/DownloadSources.py',
-                   'com/DownloadSourcesFill.py',
-                   'com/DRA.py',
-                   'com/FixPlate.py',
-                   'com/GenerateSinex.py',
-                   'com/IntegrityCheck.py',
-                   'com/LocateRinex.py',
-                   'com/NEQStack.py',
-                   'com/OTL_FES2014b.py',
-                   'com/ParallelGamit.py',
-                   'com/PlotMapView.py',
-                   'com/QueryETM.py',
-                   'com/S-score.py',
-                   'com/ScanArchive.py',
-                   'com/Stacker.py',
-                   'com/StationInfoEdit.py',
-                   'com/SyncOrbits.py',
-                   'com/TrajectoryFit.py',
-                   'com/UpdateEarthquakes.py',
-                   'com/WeeklyCombination.py',
-                   'com/Ztd2trp.py',
-                   # these are scripts that are outside dependencies but added for convenience
-                   'scripts/crz2rnx',
-                   'scripts/rnx2crz',
-                   'scripts/rename_crinex.sh',
-                   'scripts/rename_with_logs.sh',
-                   'scripts/rename_crinex2lower.sh'])
+    setup(
+    )
