e1db825ab408626ca4a60e604457214c62d2b000
Markus Hackspacher <hackspacher@gmx.de>
2024-10-18 11:46:55 +0200

    pep8 changes

--- com/ApplyCountryCode.py
+++ com/ApplyCountryCode.py
@@ -20,26 +20,34 @@ def main():
     config = pyOptions.ReadOptions('gnss_data.cfg')
     cnn = dbConnection.Cnn('gnss_data.cfg')
 
-    stations = cnn.query('SELECT * FROM stations WHERE country_code IS NULL AND '
-                         'lat IS NOT NULL and lon IS NOT NULL').dictresult()
+    stations = cnn.query('''SELECT * FROM stations
+                         WHERE country_code IS NULL AND
+                         lat IS NOT NULL and lon IS NOT NULL''').dictresult()
 
-    # add minimum delay of 1s to comply with nominatim usage policy & avoid timeout error
-    # https://operations.osmfoundation.org/policies/nominatim/
-    # https://gis.stackexchange.com/questions/331144/bulk-reverse-geocoding-with-geopy-using-built-in-rate-limiter
+    ''' add minimum delay of 1s to comply with nominatim usage policy &
+avoid timeout error
+    https://operations.osmfoundation.org/policies/nominatim/
+    https://gis.stackexchange.com/questions/331144
+/bulk-reverse-geocoding-with-geopy-using-built-in-rate-limiter
+    '''
+
     reverse = RateLimiter(geolocator.reverse, min_delay_seconds=1)
 
     for stn in stations:
         print('Station %s.%s has no ISO3 lat: %12.8f lon: %13.8f'
-              % (stn['NetworkCode'], stn['StationCode'], stn['lat'], stn['lon']))
+              % (stn['NetworkCode'], stn['StationCode'],
+                 stn['lat'], stn['lon']))
         location = reverse("%f, %f" % (stn['lat'], stn['lon']))
         if location and 'country_code' in location.raw['address'].keys():
-            ISO3 = coco.convert(names=location.raw['address']['country_code'], to='ISO3')
-            print(' -- Updating %s.%s with ISO3 %s' % (stn['NetworkCode'], stn['StationCode'], ISO3))
+            ISO3 = coco.convert(names=location.raw['address']['country_code'],
+                                to='ISO3')
+            print(' -- Updating %s.%s with ISO3 %s'
+                  % (stn['NetworkCode'], stn['StationCode'], ISO3))
             cnn.update('stations', stn, country_code=ISO3)
         else:
-            print(' -- Could not determine ISO3 for %s.%s' % (stn['NetworkCode'], stn['StationCode']))
+            print(' -- Could not determine ISO3 for %s.%s'
+                  % (stn['NetworkCode'], stn['StationCode']))
 
 
 if __name__ == '__main__':
     main()
-
