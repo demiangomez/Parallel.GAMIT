ec21f16a30af66c6a67e04add385941efbadc6e2
Leandro <soporte@dynagroup.com.ar>
2024-10-18 15:05:00 -0300

    new version

--- backend_dir/backend/backend_django_project/backend_django_project/settings.py
+++ backend_dir/backend/backend_django_project/backend_django_project/settings.py
@@ -26,6 +26,7 @@ CELERY_BROKER_URL = 'redis://localhost:6379/0'
 CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
 CELERY_ACCEPT_CONTENT = ['json']
 CELERY_TASK_SERIALIZER = 'json'
+CELERY_RESULT_EXPIRES = 3600
 
 CACHES = {
     "default": {
