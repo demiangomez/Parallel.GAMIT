88366236720edc851336438ff7eae7500549a367
Markus Hackspacher <hackspacher@gmx.de>
2024-10-28 08:24:25 +0100

    PEP8 changes CompareDBs.py

--- com/CompareDBs.py
+++ com/CompareDBs.py
@@ -6,12 +6,9 @@ import os
 import shutil
 
 # app
-from pgamit import dbConnection
-from pgamit import pyPPP
-from pgamit.Utils import stationID
+from pgamit import dbConnection, pyArchiveStruct, pyOptions, pyPPP
 from pgamit.ScanArchive import export_station
-from pgamit import pyArchiveStruct
-from pgamit import pyOptions
+from pgamit.Utils import stationID
 
 
 def main():
@@ -20,42 +17,54 @@ def main():
     cnn2 = dbConnection.Cnn('gnss_data_osu.cfg')
 
     pyArchive = pyArchiveStruct.RinexStruct(cnn1)
-    Config = pyOptions.ReadOptions("gnss_data_ign.cfg")  # type: pyOptions.ReadOptions
+    # type: pyOptions.ReadOptions
+    Config = pyOptions.ReadOptions("gnss_data_ign.cfg")
 
     # get all the stations from the database (IGN)
     stns = cnn1.query('SELECT * FROM stations').dictresult()
 
     for stn in stns:
-        sp = pyPPP.PPPSpatialCheck([float(stn['lat'])], [float(stn['lon'])], [float(stn['height'])])
+        sp = pyPPP.PPPSpatialCheck([float(stn['lat'])],
+                                   [float(stn['lon'])],
+                                   [float(stn['height'])])
 
-        found, match, stn_list = sp.verify_spatial_coherence(cnn2, stn['StationCode'])
+        found, match, stn_li = sp.verify_spatial_coherence(cnn2,
+                                                           stn['StationCode'])
 
         if found:
-            # print(' -- %s was found as %s' % (stationID(stn), stationID(match[0])))
+            # print(' -- %s was found as %s'
+            #       % (stationID(stn), stationID(match[0])))
             # if it was found don't do anything
             pass
         else:
             if len(match) > 0:
-                print(' -- %s was NOT found: maybe it is %s (distance: %8.3f m)'
-                      % (stationID(stn), stationID(match[0]), match[0]['distance']))
+                print(''' -- %s was NOT found:
+                      maybe it is %s (distance: %8.3f m)'''
+                      % (stationID(stn),
+                         stationID(match[0]),
+                         match[0]['distance']))
 
                 if not os.path.exists('export/maybe'):
                     os.makedirs('export/maybe')
 
-                export_station(cnn1, [stn], pyArchive, Config.archive_path, False)
+                export_station(cnn1, [stn], pyArchive,
+                               Config.archive_path, False)
 
                 # move the file into the folder
-                shutil.move('%s.zip' % stationID(stn), 'export/maybe/%s.zip' % stationID(stn))
+                shutil.move('%s.zip' % stationID(stn),
+                            'export/maybe/%s.zip' % stationID(stn))
             else:
                 print(' -- %s was NOT found' % stationID(stn))
 
                 if not os.path.exists('export/sure'):
                     os.makedirs('export/sure')
 
-                export_station(cnn1, [stn], pyArchive, Config.archive_path, False)
+                export_station(cnn1, [stn], pyArchive,
+                               Config.archive_path, False)
 
                 # move the file into the folder
-                shutil.move('%s.zip' % stationID(stn), 'export/sure/%s.zip' % stationID(stn))
+                shutil.move('%s.zip' % stationID(stn),
+                            'export/sure/%s.zip' % stationID(stn))
 
 
 if __name__ == '__main__':
