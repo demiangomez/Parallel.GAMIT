d457a1f068e4c4b6549767c8581bf865a0ecb477
Leandro <soporte@dynagroup.com.ar>
2024-12-12 11:31:17 -0300

    fix validation of user group

--- backend_dir/backend/backend_django_project/api/apps.py
+++ backend_dir/backend/backend_django_project/api/apps.py
@@ -24,9 +24,10 @@ class ApiConfig(AppConfig):
             raise ImproperlyConfigured(
                 "MAX_SIZE_FILE_MB must be equal or less than 75 MB")
 
-        if os.geteuid() == 0:
-            if os.system(f"getent group {settings.USER_GROUP_TO_SAVE_FILES}") != 0:
-                raise ImproperlyConfigured(
-                    f"Group  {settings.USER_GROUP_TO_SAVE_FILES} does not exist")
-        else:
-            raise ImproperlyConfigured("Not running as root")
+        import grp
+
+        try:
+            grp.getgrnam(settings.USER_GROUP_TO_SAVE_FILES)
+        except KeyError:
+            raise ImproperlyConfigured(
+                f"Group {settings.USER_GROUP_TO_SAVE_FILES} does not exist")
